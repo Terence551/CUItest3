3623ff292f6ae898c0de979bde523da2eac79013
Terence <64239529+Terence551@users.noreply.github.com>
2021-03-11 16:09:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    app.run(debug=True)
+    app.run(port=8080, debug=True)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
